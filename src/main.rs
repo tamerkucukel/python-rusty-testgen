2751911b2bc72953e555031632a9750a6b79dc19
tamerkucukel <tamerkucukel@hotmail.com>
2025-03-11 02:02:07 +0300

    Refactor, quick fixes.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,22 @@
-use rustpython_parser::{self, ast::*, Parse};
-use std::{fs::File, io::Read, path::Path};
+mod ast_analyzer;
+use ast_analyzer::ASTAnalyzer;
+use rustpython_ast::Stmt;
+use rustpython_parser::{ast, Parse};
+use std::fs;
+
+fn read_file(path: &str) -> String {
+    fs::read_to_string(path).expect("File couldn't read")
+}
+
+fn generate_ast(code: &str) -> Vec<Stmt> {
+    ast::Suite::parse_without_path(code).expect("Couldn't parse python code")
+}
+
 fn main() {
-    // Specify the path to your Python file
-    let python_file_path: &str = "./test-file.py";
-
-    // Read the file content
-    let mut file: File = File::open(Path::new(python_file_path)).expect("Could not open file");
-    let mut source_code: String = String::new();
-    file.read_to_string(&mut source_code)
-        .expect("Could not read file");
-
-    // Parse the Python code into an AST
-    let generated_ast = match Suite::parse(&source_code, &python_file_path) {
-        Ok(ast) => ast,
-        Err(err) => panic!("Failed to parse Python file: {err:?}"),
-    };
-
-    //Print AST in correct format.
-    println!("Generated ast is: {:#?}", generated_ast);
-
-    //Match with statement types.
-    for statement in generated_ast.iter() {
-        match statement {
-            Stmt::FunctionDef(StmtFunctionDef { name, .. }) => {
-                println!("Function name: {}", name);
-            }
-            _ => {}
-        }
-    }
+    let python_file_path = "./test-file.py";
+    let python_code = read_file(python_file_path);
+    let ast = generate_ast(&python_code);
+    println!("{:#?}", ast);
+    let mut analyzer = ASTAnalyzer::new();
+    analyzer.visit(&ast);
 }
